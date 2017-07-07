43a79c01ffe4b8563235200c21dda31691880396
takuto-okuno-t5 <okuno.takuto.t5@dc.tohoku.ac.jp>
2017-07-07 19:42:37 +0900

    fixed nearest neighbor process

---
+++ TPro/nnClusteringContinueDialog.m
@@ -0,0 +1,152 @@
+function varargout = nnClusteringContinueDialog(varargin)
+% NNCLUSTERINGCONTINUEDIALOG MATLAB code for nnClusteringContinueDialog.fig
+%      NNCLUSTERINGCONTINUEDIALOG, by itself, creates a new NNCLUSTERINGCONTINUEDIALOG or raises the existing
+%      singleton*.
+%
+%      H = NNCLUSTERINGCONTINUEDIALOG returns the handle to a new NNCLUSTERINGCONTINUEDIALOG or the handle to
+%      the existing singleton*.
+%
+%      NNCLUSTERINGCONTINUEDIALOG('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in NNCLUSTERINGCONTINUEDIALOG.M with the given input arguments.
+%
+%      NNCLUSTERINGCONTINUEDIALOG('Property','Value',...) creates a new NNCLUSTERINGCONTINUEDIALOG or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before nnClusteringContinueDialog_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to nnClusteringContinueDialog_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help nnClusteringContinueDialog
+
+% Last Modified by GUIDE v2.5 07-Jul-2017 18:10:03
+
+% Begin initialization code - DO NOT EDIT
+    gui_Singleton = 1;
+    gui_State = struct('gui_Name',       mfilename, ...
+                       'gui_Singleton',  gui_Singleton, ...
+                       'gui_OpeningFcn', @nnClusteringContinueDialog_OpeningFcn, ...
+                       'gui_OutputFcn',  @nnClusteringContinueDialog_OutputFcn, ...
+                       'gui_LayoutFcn',  [] , ...
+                       'gui_Callback',   []);
+    if nargin && ischar(varargin{1})
+        gui_State.gui_Callback = str2func(varargin{1});
+    end
+
+    if nargout
+        [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+    else
+        gui_mainfcn(gui_State, varargin{:});
+    end
+% End initialization code - DO NOT EDIT
+end
+
+
+% --- Executes just before nnClusteringContinueDialog is made visible.
+function nnClusteringContinueDialog_OpeningFcn(hObject, eventdata, handles, varargin)
+    % This function has no output args, see OutputFcn.
+    % hObject    handle to figure
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
+    % varargin   command line arguments to nnClusteringContinueDialog (see VARARGIN)
+
+    % Choose default command line output for nnClusteringContinueDialog
+    handles.output = hObject;
+    guidata(hObject, handles);
+
+    % UIWAIT makes nnClusteringContinueDialog wait for user response (see UIRESUME)
+    uiwait(handles.figure1);
+end
+
+% --- Outputs from this function are returned to the command line.
+function varargout = nnClusteringContinueDialog_OutputFcn(hObject, eventdata, handles) 
+    % varargout  cell array for returning output args (see VARARGOUT);
+    % hObject    handle to figure
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
+
+    % Get default command line output from handles structure
+    varargout{1} = handles.output;
+    idstr = strsplit(get(handles.edit1,'String'),',');
+    ids = [];
+    for i=1:length(idstr)
+        ids = [ids str2num(idstr{i})];
+    end
+    varargout{2} = ids;
+    varargout{3} = str2num(get(handles.edit2,'String'));
+end
+
+% --- Executes when user attempts to close figure1.
+function figure1_CloseRequestFcn(hObject, eventdata, handles)
+    % hObject    handle to figure1 (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
+    set(handles.edit2,'String', '-1');
+    uiresume(handles.figure1);
+end
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+    % hObject    handle to pushbutton1 (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
+    set(handles.edit2,'String', '-1');
+    uiresume(handles.figure1);
+end
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+    % hObject    handle to pushbutton2 (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
+    uiresume(handles.figure1);
+end
+
+
+function edit1_Callback(hObject, eventdata, handles)
+    % hObject    handle to edit1 (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
+
+    % Hints: get(hObject,'String') returns contents of edit1 as text
+    %        str2double(get(hObject,'String')) returns contents of edit1 as a double
+end
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+    % hObject    handle to edit1 (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    empty - handles not created until after all CreateFcns called
+
+    % Hint: edit controls usually have a white background on Windows.
+    %       See ISPC and COMPUTER.
+    if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+        set(hObject,'BackgroundColor','white');
+    end
+end
+
+
+function edit2_Callback(hObject, eventdata, handles)
+    % hObject    handle to edit2 (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
+
+    % Hints: get(hObject,'String') returns contents of edit2 as text
+    %        str2double(get(hObject,'String')) returns contents of edit2 as a double
+end
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+    % hObject    handle to edit2 (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    empty - handles not created until after all CreateFcns called
+
+    % Hint: edit controls usually have a white background on Windows.
+    %       See ISPC and COMPUTER.
+    if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+        set(hObject,'BackgroundColor','white');
+    end
+end
