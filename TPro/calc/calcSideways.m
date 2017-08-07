da0f464ed031f97d9eb46a7ad7aa2ea19ade3b27
takuto-okuno-t5 <okuno.takuto.t5@dc.tohoku.ac.jp>
2017-08-07 21:09:20 +0900

    fixed sideways calcuration bug.

--- TPro/calc/calcSideways.m
+++ TPro/calc/calcSideways.m
@@ -6,10 +6,11 @@ function sideways = calcSideways(x, y, dir)
     xx = [zeros(1,fly_num);xx];
     yy = diff(y);
     yy = [zeros(1,fly_num);yy];
+    yy = -yy;
 
     deg = acos((xx ./ sqrt((xx.*xx + yy.*yy)))) * (180/pi);
     deg(isnan(yy)) = NaN;
-    %  deg(yy<0) = -deg(yy<0);
+    deg(yy<0) = -deg(yy<0);
 
     sideways = abs(sin((pi/180)*(deg - dir)));
 end
