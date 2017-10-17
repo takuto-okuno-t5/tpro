0f2671fe2a5c6df2f3cfd76523bda2019ea88249
takuto-okuno-t5 <okuno.takuto.t5@dc.tohoku.ac.jp>
2017-10-17 20:28:14 +0900

    fixed config compatibility bug. changed mode_template (default detect algorithm)

--- TPro/io/checkConfigCompatibility.m
+++ TPro/io/checkConfigCompatibility.m
@@ -7,4 +7,7 @@ function outConf = checkConfigCompatibility(inConf)
     if length(outConf) < 23
         outConf = [outConf, 1, 1, 1, 0, 0, 0];
     end
+    if length(outConf) < 29
+        outConf = [outConf, 0, 0, 0, 0];
+    end
 end
