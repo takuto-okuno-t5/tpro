f29bc648568896f5726f692b0bb3b334ecd08ee8
takuto-okuno-t5 <okuno.takuto.t5@dc.tohoku.ac.jp>
2018-02-01 14:31:19 +0900

    fixed wing and head direction.

--- TPro/calc/PD_direction3.m
+++ TPro/calc/PD_direction3.m
@@ -37,15 +37,17 @@ function [ keep_direction, keep_angle, keep_wings ] = PD_direction3(step2Image, 
         end
 
         % get around color (maybe wing) colors
-        [ colors ] = getCircleColors(wingImage, cx, cy, ph, majlen * radiusRate, range, step);
-        colLen = length(colors);
+        colors(1,:) = getCircleColors(wingImage, cx, cy, ph, majlen * (radiusRate-0.1), range, step);
+        colors(2,:) = getCircleColors(wingImage, cx, cy, ph, majlen * radiusRate, range, step);
+        colors(3,:) = getCircleColors(wingImage, cx, cy, ph, majlen * (radiusRate+0.1), range, step);
+        colLen = size(colors,2);
 
-        frontTotal = sum(colors(1:floor(colLen/4))) + sum(floor(colLen/4*3)+1:colLen);
-        backTotal = sum(colors(floor(colLen/4)+1:floor(colLen/4*3)));
+        frontTotal = sum(sum(colors(:,1:floor(colLen/4)))) + sum(sum(colors(:,floor(colLen/4*3)+1:colLen)));
+        backTotal = sum(sum(colors(:,floor(colLen/4)+1:floor(colLen/4*3))));
         if frontTotal > backTotal
             vec = -vec;
             angle = angle + 180;
-            colors = [colors(floor(colLen/2)+1:colLen), colors(1:floor(colLen/2))];
+            colors = [colors(:,floor(colLen/2)+1:colLen), colors(:,1:floor(colLen/2))];
         end
         keep_direction(:,i) = vec;
 
@@ -54,39 +56,39 @@ function [ keep_direction, keep_angle, keep_wings ] = PD_direction3(step2Image, 
         colors = movmean(colors, 3);
         rstart = floor(60/step) + 1;
         rend = floor(180/step) + 1; % 19 should be 180 degree
-        wb = NaN; we = NaN;
-        for j=rstart:rend
-            if((colors(j) >= WING_COL_TH) && (colors(j+1) >= WING_COL_TH))
-                if isnan(wb)
-                    wb = j;
+        wb = nan(1,3); we = nan(1,3);
+        for k=1:size(colors,1)
+            for j=rstart:rend
+                if((colors(k,j) >= WING_COL_TH) && (colors(k,j+1) >= WING_COL_TH))
+                    if isnan(wb(k))
+                        wb(k) = j;
+                    end
+                    we(k) = j+1;
+                elseif((colors(k,j) < WING_COL_TH) && (colors(k,j+1) < WING_COL_TH)) && ~isnan(wb(k))
+                    break;
                 end
-                we = j+1;
-            elseif((colors(j) < WING_COL_TH) && (colors(j+1) < WING_COL_TH)) && ~isnan(wb)
-                break;
             end
         end
-        if ~isnan(wb) && ~isnan(we)
-            wangle = ((wb+we)/2 - 1) * step;
-            keep_wings(1,i) = angle + wangle;
-        end
+        wangle = ((wb+we)./2 - 1) .* step;
+        keep_wings(1,i) = angle + nanmean(wangle);
 
         % find left wing
         lstart = colLen + 2 - rstart;
         lend = colLen +2 - rend;
-        wb = NaN; we = NaN;
-        for j=lstart:-1:lend
-            if((colors(j) >= WING_COL_TH) && (colors(j-1) >= WING_COL_TH))
-                if isnan(wb)
-                    wb = j;
+        wb = nan(1,3); we = nan(1,3);
+        for k=1:size(colors,1)
+            for j=lstart:-1:lend
+                if((colors(k,j) >= WING_COL_TH) && (colors(k,j-1) >= WING_COL_TH))
+                    if isnan(wb(k))
+                        wb(k) = j;
+                    end
+                    we(k) = j-1;
+                elseif((colors(k,j) < WING_COL_TH) && (colors(k,j-1) < WING_COL_TH)) && ~isnan(wb(k))
+                    break;
                 end
-                we = j-1;
-            elseif((colors(j) < WING_COL_TH) && (colors(j-1) < WING_COL_TH)) && ~isnan(wb)
-                break;
             end
         end
-        if ~isnan(wb) && ~isnan(we)
-            wangle = (colLen - ((wb+we)/2 - 1)) * step;
-            keep_wings(2,i) = angle - wangle;
-        end        
+        wangle = (colLen - ((wb+we)./2 - 1)) .* step;
+        keep_wings(2,i) = angle - nanmean(wangle);
     end
 end
