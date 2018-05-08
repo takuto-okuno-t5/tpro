435145e717ba5b67fafc61c20a878db5ca48a26e
takuto-okuno-t5 <okuno.takuto.t5@dc.tohoku.ac.jp>
2018-05-08 21:10:39 +0900

    fixed weighted group count.

--- TPro/calc/calcWeightedGroupCountFrame.m
+++ TPro/calc/calcWeightedGroupCountFrame.m
@@ -32,6 +32,7 @@ function wcount = calcWeightedGroupCountFrame(tree, plen, height)
     hwidth = height / 2;
     h1 = height - hwidth;
     h2 = height + hwidth;
+    h3 = height - hwidth / 2;
     idx3 = find(dend(:,5)<=h2);
     dendIn = dend(idx3,:);
     for i=1:size(dendIn,1)
@@ -39,28 +40,29 @@ function wcount = calcWeightedGroupCountFrame(tree, plen, height)
         c2 = dendIn(i,3);
         idx1 = dendIn(1,1);
         if dendIn(i,4) == 2
-            if dendIn(i,5) >= h1
-                rate = (dendIn(i,5) - h1) / (h2 - h1);
-                dendIn(i,6) = 1 - rate;
-            else
+            if dendIn(i,5) < height  % h3
                 dendIn(i,6) = 1;
+%            elseif dendIn(i,5) < height
+%                rate = (dendIn(i,5) - h3) / (height - h3);
+%                dendIn(i,6) = 1 - rate;
+            else
+                dendIn(i,6) = 0;
             end
         elseif dendIn(i,4) == 3
-            if dendIn(i,5) >= h1
+            if dendIn(i,5) < h1
+                dendIn(i,6) = 1;
+            else
                 if c1 > plen
                     dendIn(i,6) = dendIn(c1-idx1+1, 6);
                 elseif c2 > plen
                     dendIn(i,6) = dendIn(c2-idx1+1, 6);
                 end
-            else
-                dendIn(i,6) = 1;
             end
         elseif dendIn(i,4) > 3
             if c1 > plen && c2 > plen
-                v1 = dendIn(c1-idx1+1, 6);
-                v2 = dendIn(c2-idx1+1, 6);
                 rate = (dendIn(i,5) - h1) / (h2 - h1);
-                dendIn(i,6) = 1*(1-rate) + (v1+v2)*rate;
+                halfScore = (1 + rate) / 2;
+                dendIn(i,6) = dendIn(c1-idx1+1, 6)*halfScore + dendIn(c2-idx1+1, 6)*halfScore;
             elseif c1 > plen
                 dendIn(i,6) = dendIn(c1-idx1+1, 6);
             elseif c2 > plen
