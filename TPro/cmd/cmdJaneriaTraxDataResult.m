7c67f4ad93ec72581dfe7b64dd39af4076f492a5
takuto-okuno-t5 <okuno.takuto.t5@dc.tohoku.ac.jp>
2018-07-04 17:44:50 +0900

    fixed group process of janeria trax data.

--- TPro/cmd/cmdJaneriaTraxDataResult.m
+++ TPro/cmd/cmdJaneriaTraxDataResult.m
@@ -8,31 +8,61 @@ function cmdJaneriaTraxDataResult(handles)
     tic;
 
     procName = 'dcd';
+    % find gal4 line names
     d = dir(handles.janeriaTrxPath);
     dsize = length(d);
-    data = cell(dsize, 2);
-    j = 1;
+    fnames = cell(dsize, 2);
+    folder = d(1).folder;
+    j = 1;    
     for i=1:dsize
         if d(i).isdir && ~strcmp(d(i).name,'.') && ~strcmp(d(i).name,'..')
-            fname = [d(i).folder '/' d(i).name '/registered_trx.mat'];
+            fname = [folder '/' d(i).name '/registered_trx.mat'];
             if exist(fname, 'file')
-                % load registered_trx.mat file
-                disp(['processing : ' d(i).name]);
-                [X, Y, keep_angle_sorted, keep_direction_sorted, keep_areas, keep_ecc_sorted, keep_wings_sorted, keep_data, keep_mean_blobmajor, keep_mean_blobminor, ...
-                    fps, mmperpx, startframe, endframe, maxframe] = loadJaneriaTraxMat([d(i).folder '/' d(i).name '/'], 'registered_trx.mat', 1024);
-                data{j,1} = d(i).name;
-
-                r = dcdRadius / mmperpx;
-                cnr = dcdCnRadius / mmperpx;
-
-                % calc DCD
-                means = calcLocalDensityDcd(X, Y, [], r, cnr); % empty roiMask
-                data{j,2} = nanmean(means);
+                fnames{j,1} = d(i).name;
+                C = strsplit(d(i).name, '_');
+                fnames{j,2} = C{2};
                 j = j + 1;
             end
         end
     end
-    
+    fn2 = fnames(:,2);
+    fn2(j:dsize) = [];
+    gids = findgroups(fn2);
+    gmax = max(gids);
+
+    % process registered_trx.mat files
+    data = cell(gmax, 8);
+    dsize = length(gids);
+    count = 1;
+    for i=1:gmax
+        idx = find(gids==i);
+        means = [];
+        for j=1:length(idx)
+            k = idx(j);
+
+            % load registered_trx.mat file
+            rate = count/dsize * 100;
+            disp(['processing(' num2str(count) ') : G(' num2str(i) ') ' fnames{k,1} ' (' num2str(rate) '%)']);
+            [X, Y, keep_angle_sorted, keep_direction_sorted, keep_areas, keep_ecc_sorted, keep_wings_sorted, keep_data, keep_mean_blobmajor, keep_mean_blobminor, ...
+                fps, mmperpx, startframe, endframe, maxframe] = loadJaneriaTraxMat([folder '/' fnames{k,1} '/'], 'registered_trx.mat', 1024);
+
+            r = dcdRadius / mmperpx;
+            cnr = dcdCnRadius / mmperpx;
+
+            % calc DCD
+            means1 = calcLocalDensityDcd(X, Y, [], r, cnr); % empty roiMask
+            means = [means; means1];
+            count = count + 1;
+        end
+        data{i,1} = fnames{idx(1),2};
+        data{i,2} = length(idx);
+        data{i,3} = nanmean(means);
+        data{i,4} = prctile(means,100);
+        data{i,5} = prctile(means,75);
+        data{i,6} = prctile(means,50);
+        data{i,7} = prctile(means,25);
+        data{i,8} = prctile(means,0);        
+    end
     % save data as text
     if ~isempty(handles.export)
         outputPath = [handles.export '/'];
