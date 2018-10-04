84506a57dcf698d3c0309d7bccc9c391541b34f8
takuto-okuno-t5 <okuno.takuto.t5@dc.tohoku.ac.jp>
2018-10-04 19:25:03 +0900

    fixed duration filter bug.

--- TPro/calc/beDurationFilter.m
+++ TPro/calc/beDurationFilter.m
@@ -3,10 +3,12 @@ function be_mat = beDurationFilter(mat, threshold)
     frame_num = size(mat, 1);
     fly_num = size(mat, 2);
     
-    mat(frame_num+1,:) = 0;
+    if threshold > 10 % put GAP filter last 10 frames
+        mat((frame_num-9):frame_num,:) = 1;
+    end
     for fn = 1:fly_num
         cnt = 0;
-        for i = 1:(frame_num + 1)
+        for i = 1:frame_num
             if mat(i,fn) > 0
                 cnt = cnt + 1;
             elseif cnt > 0
@@ -17,6 +19,12 @@ function be_mat = beDurationFilter(mat, threshold)
             end
         end
     end
-    mat(frame_num+1,:) = [];
+    if threshold > 10 % clear GAP filter last 10 frames
+        for fn = 1:fly_num
+            if mat(frame_num-10,fn) == 0
+                mat((frame_num-9):frame_num,fn) = 0;
+            end
+        end
+    end
     be_mat = mat;
 end
