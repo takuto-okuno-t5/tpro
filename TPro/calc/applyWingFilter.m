cb6153e34e7b66eb4c5e2ef72576cfa497261acf
takuto-okuno-t5 <okuno.takuto.t5@dc.tohoku.ac.jp>
2020-02-07 18:48:49 +0900

    fixed body-side detection.

--- TPro/calc/applyWingFilter.m
+++ TPro/calc/applyWingFilter.m
@@ -1,13 +1,13 @@
 %%
 function wingImage = applyWingFilter(img, wingColorMin, wingColorMax)
     wingImage = img;
-    wingImage(wingImage >= wingColorMax) = 255;
-    wingImage(wingImage <= wingColorMin) = 255;
+    wingImage(wingImage > wingColorMax) = 255;
+    wingImage(wingImage < wingColorMin) = 255;
     wingImage = 255 - wingImage;
     wingImage(wingImage > 0) = 255;
 
     % blur and cut again
     wingImage = imgaussfilt(wingImage, 1);
-    wingImage(wingImage <= wingColorMin) = 0;
-    wingImage(wingImage > wingColorMin) = 255;
+    wingImage(wingImage < wingColorMin) = 0;
+    wingImage(wingImage >= wingColorMin) = 255;
 end
